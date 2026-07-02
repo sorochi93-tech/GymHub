44cc1eb4a312aa1c396414bb2cb0356748ab1f9e
Sorochi <sorochi93@gmail.com>
2026-07-02 19:18:23 +0100

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+
+const btn = document.getElementById9('menu-btn');
+const responsive.addEventListener('click', () => {
+
+    // 1. Toggle the mobile menu visibility
+    btn.classList.toggle('open');
+});
