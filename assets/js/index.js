1dc5f24fa124d4fe07ce4e015fa85c5898eacfff
Sorochi <sorochi93@gmail.com>
2026-08-27 18:37:56 +0100

    scroll2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,23 @@
+const ScrollToTop = document.getElementById("ScrollToTopSection");
 
 const toggleButton = document.getElementById("toggle-button");
 const dropdown = document.getElementById("dropdown");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    ScrollToTop.classList.remove("hidden");
+  } else {
+    ScrollToTop.classList.add("hidden");
+  }
+});
+
+ScrollToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
 toggleButton.addEventListener("click", () => {
-    dropdown.classList.toggle("hidden");
+  dropdown.classList.toggle("hidden");
 });
