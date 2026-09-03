e43e9f0675f3d223330e1600d8aa72e0c4dfdb85
Sorochi <sorochi93@gmail.com>
2026-09-03 14:58:23 +0100

    commitscroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,30 @@
-const ScrollToTop = document.getElementById("ScrollToTopSection");
+const scrollToTopDiv = document.getElementById("scrollToTopSection");
 
 const toggleButton = document.getElementById("toggle-button");
 const dropdown = document.getElementById("dropdown");
 
+
+
+const scrollToTopSection = document.getElementById("scrollToTopSection");
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    ScrollToTop.classList.remove("hidden");
-  } else {
-    ScrollToTop.classList.add("hidden");
-  }
+  scrollToTopSection.classList.toggle("hidden", window.scrollY <= 300);
 });
 
-ScrollToTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 300) {
+//     scrollToTopDiv.classList.remove("hidden");
+//   } else {
+//     scrollToTopDiv.classList.add("hidden");
+//   }
+// });
+
+// scrollToTopDiv.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
 
 toggleButton.addEventListener("click", () => {
   dropdown.classList.toggle("hidden");
